7f9897b49624cd3f29601389361cf0a3b01fb0e7
Nikolrusik <nikolrusik@gmail.com>
2021-11-27 11:40:36 +0300

    Added 3 tasks in Homework 1 on lesson 1

---
+++ PythonBasic/Ruslan_Nikolenko_dz_1/task3.py
@@ -0,0 +1,15 @@
+name_str = "процент"
+count = 0
+while True:
+    if count < 100:
+        count += 1
+        a = str(count)
+        if a[-1] == '1' and (count < 11 or count > 19):
+            print(count, 'процент')
+        elif a[-1] != '1' and (count < 11 or count > 19) and int(a[-1]) < 5 and a[-1] != '0':
+            print(count, 'процента')
+        elif a[-1] != '1' and (int(a[-1]) >= 5 or int(a[-1]) == 0 or (count > 11 or count < 19)):
+            print(count, 'процентов')
+    else:
+        break
+
